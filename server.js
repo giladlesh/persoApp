87f7b3c8b7c3e80e3fd4c56e20f2d26bd1bcfc18
Gilad Lesh <giladlesh@gmail.com>
2019-01-05 22:16:47 +0200

    json restructure

--- server.js
+++ server.js
@@ -2,105 +2,8 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var middleware = require('./middleware.js');
+var jsonSample = require('./jsonSample.js');
 
-
-var json = {
-    formID: "firstForm",
-    questions: [
-      {
-        questionID: "Q00001",
-        questionTag: "isMarried",
-        order: 1,
-        type: "american",
-        questionLabel: "Are you married?",
-        answers: [
-          {
-            key: 1,
-            answerLabel: "Yes"
-          },
-          {
-            key: 2,
-            answerLabel: "No"
-          },
-          {
-            key: 3,
-            answerLabel: "Else",
-            nextQuestion: 3
-          }
-        ]
-      },
-      {
-        questionID: "Q00002",
-        questionTag: "isMarried",
-        order: 2,
-        type: "american",
-        questionLabel: "Are you married?",
-        answers: [
-          {
-            key: 1,
-            answerLabel: "Yes"
-          },
-          {
-            key: 2,
-            answerLabel: "No"
-          }
-        ]
-      },
-      {
-        questionID: "Q00003",
-        order: 3,
-        type: "american",
-        questionTag: "skippedQuestion",
-        questionLabel: "Did you skip a question?",
-        answers: [
-          {
-            key: 1,
-            answerLabel: "Yes"
-          },
-          {
-            key: 2,
-            answerLabel: "No"
-          }
-        ]
-      },
-      {
-        questionID: "Q00004",
-        order: 4,
-        type: "datePicker",
-        questionLabel: "Please enter informations about the army",
-        answers: [
-          {
-            key: 1,
-            answerLabel: "Army started on",
-            answerTag: "armyStartingDate"
-          },
-          {
-            key: 2,
-            answerLabel: "Army ended on",
-            answerTag: "armyEndDate"
-          }
-        ]
-      },
-      {
-        questionID: "Q00005",
-        order: 5,
-        type: "input",
-        questionLabel: "Please enter following information",
-        answers: [
-          {
-            key: 1,
-            answerLabel: "First name",
-            answerTag: "firstName"
-          },
-          {
-            key: 3,
-            answerLabel: "Last name",
-            answerTag: "lastName"
-          }
-        ]
-      }
-    ]
-  };
 
 app.use(middleware.logger);
 
@@ -109,7 +12,7 @@ app.get('/about', middleware.requireAuthentication, function (req, res) {
 });
 
 app.get('/jsonSample', middleware.requireAuthentication, function (req, res) {
-	res.send(json);
+	res.send(jsonSample);
 });
 
 app.use(express.static(__dirname + '/public'));
